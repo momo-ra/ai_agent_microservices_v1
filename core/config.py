1df4dd29aa59ef6c8660505ceaf2aa0059d77170
Mohamed Ramadan <mohamedramadan@Mohameds-MacBook-Air.local>
2025-05-20 15:25:13 +0200

    change to local timescaledb

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv('.env', override=True)
 
 class Settings:
     DB_USER = os.getenv('DB_USER')
